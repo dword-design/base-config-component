0d93ddade7682812807b5d3044c24c23feaaf214
dword <info@sebastianlandwehr.com>
2021-06-14 14:19:32 +0000

    chore: fix tests

--- src/base-config.spec.js
+++ src/base-config.spec.js
@@ -1,7 +1,7 @@
 import tester from '@dword-design/tester'
 import testerPluginTmpDir from '@dword-design/tester-plugin-tmp-dir'
 import { outputFile } from 'fs-extra'
-import stealthyRequire from 'stealthy-require'
+import stealthyRequire from 'stealthy-require-no-leak'
 
 export default tester(
   {
