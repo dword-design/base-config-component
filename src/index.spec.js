2d3e6e12d19ec80990e16dc9ab217607951b4249
Sebastian Landwehr <info@sebastianlandwehr.com>
2021-06-27 12:48:12 +0200

    fix: tests (#48)

---
+++ src/index.spec.js
@@ -0,0 +1,148 @@
+import chdir from '@dword-design/chdir'
+import { endent } from '@dword-design/functions'
+import puppeteer from '@dword-design/puppeteer'
+import tester from '@dword-design/tester'
+import testerPluginTmpDir from '@dword-design/tester-plugin-tmp-dir'
+import execa from 'execa'
+import fileUrl from 'file-url'
+import { mkdir, outputFile, remove } from 'fs-extra'
+import { Builder, Nuxt } from 'nuxt'
+import outputFiles from 'output-files'
+
+export default tester(
+  {
+    component: async () => {
+      await outputFiles({
+        'pages/index.vue': endent`
+          <template>
+            <tmp-component class="tmp-component" />
+          </template>
+
+          <script>
+          import TmpComponent from '../../tmp-component'
+
+          export default {
+            components: {
+              TmpComponent,
+            },
+          }
+          </script>
+        `,
+      })
+
+      const nuxt = new Nuxt()
+      await new Builder(nuxt).build()
+      await nuxt.listen()
+
+      const browser = await puppeteer.launch()
+
+      const page = await browser.newPage()
+      try {
+        await page.goto('http://localhost:3000')
+
+        const component = await page.waitForSelector('.tmp-component')
+        expect(await component.evaluate(el => el.innerText)).toEqual(
+          'Hello world'
+        )
+      } finally {
+        await browser.close()
+        await nuxt.close()
+      }
+    },
+    plugin: async () => {
+      await outputFiles({
+        'pages/index.vue': endent`
+          <template>
+            <tmp-component class="tmp-component" />
+          </template>
+        `,
+        'plugins/plugin.js': endent`
+          import Vue from 'vue'
+          import MyComponent from '../../tmp-component'
+          
+          Vue.use(MyComponent)
+        `,
+      })
+
+      const nuxt = new Nuxt({ plugins: ['~/plugins/plugin.js'] })
+      await new Builder(nuxt).build()
+      await nuxt.listen()
+
+      const browser = await puppeteer.launch()
+
+      const page = await browser.newPage()
+      try {
+        await page.goto('http://localhost:3000')
+
+        const component = await page.waitForSelector('.tmp-component')
+        expect(await component.evaluate(el => el.innerText)).toEqual(
+          'Hello world'
+        )
+      } finally {
+        await browser.close()
+        await nuxt.close()
+      }
+    },
+    script: async () => {
+      await outputFile(
+        'index.html',
+        endent`
+        <body>
+          <script src="https://unpkg.com/vue"></script>
+          <script src="../tmp-component/dist/index.min.js"></script>
+        
+          <div id="app">
+            <tmp-component class="tmp-component" />
+          </div>
+        
+          <script>
+            new Vue({ el: '#app' })
+          </script>
+        </body>
+      `
+      )
+
+      const browser = await puppeteer.launch()
+
+      const page = await browser.newPage()
+      try {
+        await page.goto(fileUrl('index.html'))
+
+        const component = await page.waitForSelector('.tmp-component')
+        expect(await component.evaluate(el => el.innerText)).toEqual(
+          'Hello world'
+        )
+      } finally {
+        await browser.close()
+      }
+    },
+  },
+  [
+    {
+      after: () => remove('tmp-component'),
+      before: async () => {
+        await mkdir('tmp-component')
+        await chdir('tmp-component', async () => {
+          await outputFiles({
+            'node_modules/base-config-self/index.js':
+              "module.exports = require('../../../src')",
+            'package.json': JSON.stringify({
+              baseConfig: 'self',
+              name: 'tmp-component',
+            }),
+            'src/index.vue': endent`
+              <script>
+              export default {
+                render: () => <div>Hello world</div>
+              }
+              </script>
+            `,
+          })
+          await execa.command('base prepare')
+          await execa.command('base prepublishOnly')
+        })
+      },
+    },
+    testerPluginTmpDir(),
+  ]
+)
